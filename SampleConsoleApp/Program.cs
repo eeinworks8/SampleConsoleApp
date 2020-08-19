cfad21a540d86cac40054555cfd9752c29c7289f
Brandon Smith <Brandon.Smith@centurylink.com>
2020-08-19 07:01:34 -0400

    commit 2

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -8,6 +8,7 @@ namespace SampleConsoleApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Commit 1");
+            Console.WriteLine("Commit 2");
         }
     }
 }
