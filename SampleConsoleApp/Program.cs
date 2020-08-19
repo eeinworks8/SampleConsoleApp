344cbc8cfc6b46850a9f124fc8edd0a7407cc2e3
Brandon Smith <Brandon.Smith@centurylink.com>
2020-08-19 07:40:45 -0400

    change2

--- SampleConsoleApp/Program.cs
+++ SampleConsoleApp/Program.cs
@@ -10,8 +10,9 @@ namespace SampleConsoleApp
             Console.WriteLine("Commit 1");
             Console.WriteLine("Commit 2");
             Console.WriteLine("Commit 3");
-            
+
             // Change local master copy
+            // Change local master copy2
         }
     }
 }
